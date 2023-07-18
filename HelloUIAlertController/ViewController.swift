18bdee7ded3c618085306134876234a833081349
Richy <richy@webcomm.com.tw>
2023-07-18 15:20:58 +0800

    1.new Alert Action 2.dismiss消失Alert 3.addAction

--- HelloUIAlertController/ViewController.swift
+++ HelloUIAlertController/ViewController.swift
@@ -17,14 +17,13 @@ class ViewController: UIViewController {
                                         message: "How are you?",
                                         preferredStyle: UIAlertController.Style.alert)
         
-        // define alert button 1
+        // UIAlertAction // define alert button 1
         let okAction = UIAlertAction(title: "OK", style: .default) {
             (action:UIAlertAction) in
                 self.dismiss(animated: true, completion: nil)
         }
-        // let okAction = UIAlertAction(title: "OK", style: .default) // 點擊後無觸發事件
         
-        // define alert button 2
+        // UIAlertAction // define alert button 2
         let helloAction = UIAlertAction(title: "Say Hello", style: .destructive) {
             (action:UIAlertAction) in
             print("Hello")
@@ -38,7 +37,9 @@ class ViewController: UIViewController {
             self.dismiss(animated: true, completion: nil)
         }
         
-        // alert add button
+        // let okAction = UIAlertAction(title: "OK", style: .default) // 點擊後無觸發事件
+        
+        // Alert Controller add UIAlertAction (button)
         myAlert.addAction(okAction)
         myAlert.addAction(helloAction)
         myAlert.addAction(cancelAction)
